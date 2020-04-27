aed89af819481c08487764bb913ba0eb3206b120
itexpert120 <itexpert120@gmail.com>
2020-04-27 12:18:04 +0200

    remove out dir

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 7 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 SMP PREEMPT Mon Apr 27 12:06:35 CEST 2020"
-#define LINUX_COMPILE_BY "zeeshan"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "gcc version 5.4.0 20160609 (Ubuntu/Linaro 5.4.0-6ubuntu1~16.04.9) "
